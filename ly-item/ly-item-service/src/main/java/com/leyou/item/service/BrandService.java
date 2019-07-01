2fb16673034f7f0e36c133d050da32cfaff88c18
fdm <245211612@qq.com>
2019-07-01 10:25:35 +0800

    brand分页查询

--- ly-item/ly-item-service/src/main/java/com/leyou/item/service/BrandService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/BrandService.java
@@ -1,11 +1,15 @@
 package com.leyou.item.service;
 
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+//import com.github.pagehelper.util.StringUtil;
 import com.leyou.common.pojo.PageResult;
 import com.leyou.item.mapper.BrandMapper;
 import com.leyou.item.pojo.Brand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+import tk.mybatis.mapper.util.StringUtil;
 
 @Service
 public class BrandService {
@@ -13,16 +17,23 @@ public class BrandService {
     @Autowired
     BrandMapper brandMapper;
 
-//    public PageResult<Brand> queryBrandByPageAndSort(
-//            Integer page, Integer rows, String sortBy, Boolean desc, String key
-//    ) {
-//        // 开始分页
-//        PageHelper.startPage(page, rows);
-//        // 过滤
-//
-//        // 排序
-//
-//        // 查询
-//
-//    }
+    public PageResult<Brand> queryBrandByPageAndSort(
+            Integer page, Integer rows, String sortBy, Boolean desc, String key
+    ) {
+        // 开始分页
+        PageHelper.startPage(page, rows);
+        // 过滤
+        Example example = new Example(Brand.class);
+        if (StringUtil.isNotEmpty(key)) {
+            example.createCriteria().andLike("name", "%" + key + "%").orEqualTo("letter", key);
+        }
+        // 排序
+        if (StringUtil.isNotEmpty(sortBy)) {
+            String orderByClause = sortBy + (desc ? " DESC" : " ASC");
+            example.setOrderByClause(orderByClause);
+        }
+        // 查询
+        Page<Brand> pageInfo = (Page<Brand>) brandMapper.selectByExample(example);
+        return new PageResult<Brand>(pageInfo.getTotal(), pageInfo);
+    }
 }
